502fd88b4950f74263f73765408ee21805d37d21
Andrea Francia <andrea@andreafrancia.it>
2014-03-09 14:21:33 +0100

    Now links with Assimp library

--- howto-link-assimp-library-in-osx-app/main.m
+++ howto-link-assimp-library-in-osx-app/main.m
@@ -13,10 +13,9 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
         // insert code here...
         NSLog(@"Hello, World!");
-        
+        aiImportFile("file name", 0);
     }
     return 0;
 }
