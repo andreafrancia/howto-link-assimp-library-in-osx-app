99d4edac6a3182839a6da8986a3c40400a328db7
Andrea Francia <andrea@andreafrancia.it>
2014-03-09 14:13:20 +0100

    Hello world

---
+++ howto-link-assimp-library-in-osx-app/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  howto-link-assimp-library-in-osx-app
+//
+//  Created by Andrea Francia on 3/9/14.
+//  Copyright (c) 2014 Andrea Francia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
